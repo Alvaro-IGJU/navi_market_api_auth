d62fd8a3a504d26fc9ac4a6d8a18a103a92354a6
alvaro-IGJU <alvaro.iglesias.dev@gmail.com>
2024-12-14 11:46:19 +0100

    email settings

--- navi_market_api_auth/settings.py
+++ navi_market_api_auth/settings.py
@@ -114,8 +114,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # O tu proveedor de correo
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alvaro.iglesias.dev@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sa66Ji01'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 AUTH_USER_MODEL = 'users.User'
 
