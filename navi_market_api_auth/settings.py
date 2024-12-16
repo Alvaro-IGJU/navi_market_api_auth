1950fd735bd30753e4b2315e2e1e22ffaa66f90d
alvaro-IGJU <alvaro.iglesias.dev@gmail.com>
2024-12-16 08:20:08 +0100

    change to mysql bye bye sqlite

--- navi_market_api_auth/settings.py
+++ navi_market_api_auth/settings.py
@@ -97,12 +97,19 @@ WSGI_APPLICATION = 'navi_market_api_auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'navi_market',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # O la IP del servidor de la base de datos
+        'PORT': '3306',
     }
 }
+
+
 
 AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailOrUsernameModelBackend',  # Backend personalizado
