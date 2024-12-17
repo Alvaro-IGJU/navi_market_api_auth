d1f38304503e05135de0df248fa37810ff976a83
alvaro-IGJU <alvaro.iglesias.dev@gmail.com>
2024-12-17 12:00:52 +0100

    working on dashboards

--- interactions/urls.py
+++ interactions/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("register/<int:stand_id>/", RegisterInteractionView.as_view(), name="register-interaction"),
     path("update-duration/<int:interaction_id>/", UpdateInteractionDurationView.as_view(), name="update-interaction-duration"),
     path("companies/<int:company_id>/interactions/", CompanyInteractionsView.as_view(), name="company-interactions"),
+    path("companies/<int:company_id>/events-visits/", CompanyEventVisitsSummaryView.as_view(), name="company-events-visits"),
+
 ]
