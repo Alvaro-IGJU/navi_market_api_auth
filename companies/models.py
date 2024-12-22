2f0be04964c23277fd6047065ef42c50bc09d6ab
salimtamaichi <salim.tamazirt.dev@gmail.com>
2024-12-22 21:50:10 +0100

    EventRetrieveView update

--- companies/models.py
+++ companies/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from users.models import Sector
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
-    sector = models.CharField(max_length=255, blank=True, null=True)
+    sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, null=True, blank=True, related_name="companies")  # Relación con Sector
     contact_email = models.EmailField()
     contact_phone = models.CharField(max_length=50, blank=True, null=True)
     website = models.URLField(blank=True, null=True)
--- events/views/events/EventRetrieveView.py
+++ events/views/events/EventRetrieveView.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import  AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from events.models import Event
 from events.serializers import EventSerializer
@@ -9,14 +9,32 @@ from rest_framework.response import Response
 class EventRetrieveView(APIView):
     """
     Vista para obtener los detalles de un evento específico (Acceso público).
+    Devuelve la información del evento junto con los sectores únicos de las empresas participantes.
     """
     permission_classes = [AllowAny]
 
     def get(self, request, pk):
         try:
+            # Obtener el evento específico
             event = Event.objects.get(pk=pk)
-            serializer = EventSerializer(event)
-            return Response(serializer.data)
-        except Event.DoesNotExist:
-            return Response({'error': 'Evento no encontrado'}, status=status.HTTP_404_NOT_FOUND)
 
+            # Serializar los detalles del evento
+            serializer = EventSerializer(event)
+
+            # Obtener los sectores únicos de las empresas asociadas a través de stands
+            stands = event.stands.all()  # Asume que hay una relación stands en Event
+            companies = [stand.company for stand in stands if stand.company]
+            print(companies, "AAAAAAAAAAAA")
+            sectors = {company.sector for company in companies if company.sector and company.sector}
+
+            # Formar la respuesta combinada
+            response_data = {
+                "event": serializer.data,
+                "unique_sectors": list(sectors),
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        except Event.DoesNotExist:
+            return Response(
+                {"error": "Evento no encontrado"}, status=status.HTTP_404_NOT_FOUND
+            )
